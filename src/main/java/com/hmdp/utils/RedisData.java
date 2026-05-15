b429b2813d040772109920b6333cb58cde3cc41d
itcast <x123450000@qq.com>
2026-05-15 17:01:25 +0800

    根据id查询商铺的缓存击穿处理--逻辑过期方案

--- src/main/java/com/hmdp/utils/RedisData.java
+++ src/main/java/com/hmdp/utils/RedisData.java
@@ -1,10 +1,12 @@
 package com.hmdp.utils;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
 
 @Data
+@Accessors(chain = true)
 public class RedisData {
     private LocalDateTime expireTime;
     private Object data;
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,22 @@
 package com.hmdp;
 
+import com.hmdp.service.IShopService;
+import com.hmdp.service.impl.ShopServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+
+    @Resource
+    private ShopServiceImpl shopServiceImpl;
+
+    @Test
+    void testSaveShop() throws InterruptedException {
+        shopServiceImpl.saveShop2Redis(1L, 10L);
+    }
 
 
 }
